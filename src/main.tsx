caecbcded9b245c2e728c608544bc76285993220
slo0ey <rurchi1206@gmail.com>
2025-08-30 07:59:29 +0900

    update: tailwindcss 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import {
   createRootRoute,
   createRoute,
   createRouter,
-} from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-
-import "./styles.css";
+} from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 
 const rootRoute = createRootRoute({
   component: () => (
@@ -22,7 +20,7 @@ const rootRoute = createRootRoute({
 
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: "/",
+  path: '/',
 });
 
 const routeTree = rootRoute.addChildren([indexRoute]);
@@ -30,19 +28,19 @@ const routeTree = rootRoute.addChildren([indexRoute]);
 const router = createRouter({
   routeTree,
   context: {},
-  defaultPreload: "intent",
+  defaultPreload: 'intent',
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
 });
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
   }
 }
 
-const rootElement = document.getElementById("app");
+const rootElement = document.getElementById('app');
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
