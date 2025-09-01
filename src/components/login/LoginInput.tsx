9d26d88027a3f7946d4b2680dd273196e5c3eb52
slo0ey <rurchi1206@gmail.com>
2025-09-01 09:07:59 +0900

    feat: LoginInput 컴포넌트 추가

---
+++ src/components/login/LoginInput.tsx
@@ -0,0 +1,25 @@
+import type React from 'react';
+
+type LoginInputProps = {
+  type: string;
+  placeholder: string;
+  name: string;
+  value?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+function LoginInput({ type, placeholder, name, value, onChange }: LoginInputProps) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      required
+      onChange={onChange}
+      className="bg-bg-primary-dark light:bg-bg-elevated-light text-text-primary-dark light:text-text-primary-light rounded p-2 text-sm"
+    />
+  );
+}
+
+export default LoginInput;
